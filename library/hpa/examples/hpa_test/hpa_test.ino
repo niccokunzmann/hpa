6916e6f33edea717e15df18c820830680fd962e7
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-02-03 19:38:52 +0100

    added example

---
+++ library/hpa/examples/hpa_test/hpa_test.ino
@@ -0,0 +1,18 @@
+
+#include "hpa.h"
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  
+  HPA::xreal x = HPA::xreal(12.0);
+  
+  Serial.println(x._2double());
+  Serial.println(HPA::xONE._2float());
+  Serial.println(HPA::xTWO._2float());
+  Serial.println(sizeof(short));
+}
+
+void loop () {
+  
+};
